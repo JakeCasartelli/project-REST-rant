9a4d0fe00ca2dd4550ed176c98ce6d9680396f77
Jake Casartelli <casartellijake@yahoo.com>
2022-10-24 11:55:15 -0400

    edit pics

--- views/home.jsx
+++ views/home.jsx
@@ -9,7 +9,12 @@ function home () {
               <a href="/places">
                 <button className="btn-primary">Places Page</button>
               </a>
-
+              <div>
+                <img src="/images/wheat.jpg" alt="wheat field" />
+              <div>
+                Photo by <a href="https://unsplash.com/@saadchdhry?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Saad Chaudhry</a> on <a href="https://unsplash.com/s/photos/free?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+              </div>
+            </div>
           </main>
       </Def>
     )
