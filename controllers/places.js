e14070e4fb2a46577fa248c8c31d1d5e65d37b01
Jake Casartelli <casartellijake@yahoo.com>
2022-10-24 18:15:28 -0400

    fixed

--- controllers/places.js
+++ controllers/places.js
@@ -2,19 +2,6 @@ const router = require('express').Router()
 const places = require('../models/places.js')
 
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/rest1.jpg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/rest2.jpg'
-      }]
       res.render('places/index', {places})
 })
 
